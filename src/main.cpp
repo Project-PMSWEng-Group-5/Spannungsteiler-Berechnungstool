de63137e7108e4fd21a4b60d68cd5e540a305e84
Flurin Arquint <flurin.arquint@hsr.ch>
2019-11-12 15:54:01 +0100

    Added function-header for calculating E-Series value

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <QApplication>
 #include "../include/MainWindow.h"
+#include <cmath>        // std::abs
+
+const double E3[] = {1.0, 2.2, 4.7};
+const double E6[] = {1.0, 1.5, 2.2, 3.3, 4.7, 6.8};
+const double E12[] = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2};
+const double E24[] = {1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1};
+
+
+/* DESCRIPTION :    Evaluates number of choosen E-Series which matches the best the given value
+* INPUTS :          double value    -> value of which closest E-Series number should be evaluated
+*                   double* eSeries -> Pointer to the choosen E-Series
+*                   int size        -> Size of the choosen E-Series-Array
+* OUTPUTS :         double number   -> evaluated E-Series number
+*/
+double findClosest( double value, double* eSeries, int size);
 
 int main(int argc, char* argv[])
 {
