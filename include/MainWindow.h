ebfe801f2fb12792e131fe83a094a30e725b58a0
BrotBroesmeli <simeon.roth@hsr.ch>
2019-12-12 02:00:23 -0800

    added include-path to Doxyfile, documented MainWindow.h

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -8,11 +8,16 @@ namespace Ui
 class MainWindow;
 }
 
+/*! \brief Qt MainWindow
+ *  Builds the GUI for the "Spannungsteiler-Berechnungstool".
+ */
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
  public:
+  /*! \brief Creates the main window object.
+   *  \param parent parent object */
   explicit MainWindow(QWidget* parent = nullptr);
   ~MainWindow();
 
